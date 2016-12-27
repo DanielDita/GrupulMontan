b1491c097980b0b8a3620dd8c5c47609b706f1cc
Daniel Dita <dita.daniel@gmail.com>
2016-12-28 00:59:30 +0200

    fix for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
