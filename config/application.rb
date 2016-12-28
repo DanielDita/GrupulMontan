8c45b43051d7646b7cc81123a5e2c5562d1f82f4
Daniel Dita <dita.daniel@gmail.com>
2016-12-28 12:55:35 +0200

    hope I fixed css

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Blog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,6 +23,6 @@ module Blog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile += %w(application.js)
+
   end
 end
