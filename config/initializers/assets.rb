f132697555ce5a5271bf32e3da8de0ee2a5a9c63
Daniel Dita <dita.daniel@gmail.com>
2017-01-04 00:30:49 +0200

    Minor fixes for articles part 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( articles.css )
 Rails.application.config.assets.precompile += %w( pins.js )
+Rails.application.config.assets.precompile += %w( application.js )
 # Add additional assets to the asset load path
 
 # Rails.application.config.assets.paths << Emoji.images_path
